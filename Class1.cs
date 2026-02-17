75115bb6cf1dd01855090659c895ca4fb488c4bf
Allen Espanola <allen.espanola@outlook.com>
2026-02-17 13:07:01 -0800

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Assignment_6._1
+{
+    enum HouseType
+    {
+        Ranch, 
+        Craftsman, 
+        Victorian, 
+        Colonial,
+        Modern
+    }
+    internal class House
+    {
+        public int HouseNumber { get; set; }
+        public string StreetName { get; set; }
+        public string CityName { get; set; }
+        public HouseType HouseType { get; set; }
+
+        public House next;
+        public House(int houseNumber, string streetName, string cityName, HouseType houseType) 
+        {
+            HouseNumber = houseNumber;
+            StreetName = streetName;
+            CityName = cityName;
+            HouseType = houseType;
+            next = null;
+        }
+    }
+
+    class LinkdList
+    {
+        private House head;
+        private House tail;
+        private int size;
+
+        
+
+        public LinkdList()
+        {
+            head = null;
+            tail = null;
+            size = 0;
+        }
+
+        public bool IsEmpty()
+        {
+            return size == 0;
+        }
+
+        public void Add(int houseNumber, string streetName, string cityName, HouseType houseType)
+        {
+            House newHouse = new House(houseNumber, streetName, cityName, houseType);
+            if (IsEmpty())
+            {
+                this.head = newHouse;
+                this.tail = newHouse;
+            }
+            else
+            {
+                newHouse.next = head;
+                head = newHouse;
+            }
+            size++;
+        }
+
+        public void Display()
+        {
+            House temp = head;
+            if (IsEmpty())
+            {
+                Console.WriteLine("House list is Empty.");
+                return;
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    if (temp != null)
+                    {
+                        Console.WriteLine($"{temp.HouseNumber} {temp.StreetName}, {temp.CityName} | House Type: {temp.HouseType}"); 
+                    }
+                    temp = temp.next;
+                }
+            }
+        }
+
+        public void Search(string streetName, int? number = null)
+        {
+            House temp = head;
+
+            if (IsEmpty())
+            {
+                Console.WriteLine("House list is Empty.");
+                return;
+            }
+
+            Console.WriteLine("\nHouse information:");
+            while (temp != null)
+            {
+                if (number == null && temp.StreetName == streetName)
+                {
+                    Console.WriteLine($"{temp.HouseNumber} {temp.StreetName}");
+                    Console.WriteLine(temp.CityName);
+                    Console.WriteLine($"House Type: {temp.HouseType}");
+                    return;
+                }
+                if (number != null && temp.HouseNumber == number && temp.StreetName == streetName)
+                {
+                    Console.WriteLine($"{temp.HouseNumber} {temp.StreetName}");
+                    Console.WriteLine(temp.CityName);
+                    Console.WriteLine($"House Type: {temp.HouseType}");
+                    return;
+                }
+
+                temp = temp.next;
+            }
+            Console.WriteLine("No records found!");
+
+
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Assignment_6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----- Linked List -----");
+            LinkdList myHouse = new LinkdList();
+            myHouse.Add(2456, "Street Name", "City Name", HouseType.Ranch);
+            myHouse.Add(1234, "Road Name", "Another City", HouseType.Victorian);
+            myHouse.Add(4567, "Lane Name", "My City", HouseType.Colonial);
+            myHouse.Display();
+            Console.WriteLine("\nSearch Records for address name: Road Name");
+            myHouse.Search("Road Name");
+
+            Console.WriteLine("\n----- Move Zeroes -----");
+            int[] nums = [0, 1, 0, 3, 12];
+            Console.Write($"Original array: [");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write(i < nums.Length - 1 ? $"{nums[i]}, " : $"{nums[i]}]\n");
+            }
+            MoveZero(nums);
+            
+            Console.Write("Altered array: [");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write(i < nums.Length -1 ? $"{nums[i]}, " : $"{nums[i]}]\n");
+            }
+
+        }
+
+        public static int[] MoveZero(int[] nums)
+        {
+            for (int i = 0, j = 1; j < nums.Length;)
+            {
+                if (nums[i] > 0)
+                    i++;
+
+                if (nums[i] == 0)
+                {
+                    if (nums[j] > 0)
+                    {
+                        nums[i] = nums[j];
+                        nums[j] = 0;
+                    }
+                    j++;
+                }    
+            }
+
+            return nums;
+        }
+
+    }
+}
